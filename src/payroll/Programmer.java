4a50a9b6c0316f1ab07fe65a7d4907b269d4f6ea
hp <hp@172.17.87.3>
2019-07-22 11:18:39 +0530

    EMPLOYEE PAY SLIP

---
+++ src/payroll/Programmer.java
@@ -0,0 +1,46 @@
+package payroll;
+
+public class Programmer extends Employee{
+	private double basicpay;
+    
+	public Programmer()
+	{
+		basicpay=0;
+	}
+    
+	public Programmer (String n,long id,String ad, String mail, long mo,double BP)
+	{
+		super (n,id,ad,mail,mo);
+		basicpay=BP;
+	}
+    
+	public void printAccount()
+	{
+		super.printAccount ();
+		System.out.println("Basic pay:"+basicpay);
+	}
+	public void printStatement()
+	{
+    	double total;
+    	double total1;
+    	double total2;
+    	double total3;
+    	double gross;
+    	double net;
+    	printAccount();
+    	
+    	
+    	total=basicpay*0.97;
+    	total1=basicpay*0.1;
+    	total2=basicpay*0.12;
+    	total3=basicpay*0.001;
+    	gross=total+total1+total2+total3;
+    	net=gross-total2-total3;
+    	System.out.printf("Duty Allowance (DA):%2f\n",total);
+    	System.out.printf("HRA:%2f\n",total1);
+    	System.out.printf("PF:%2f\n",total2);
+    	System.out.printf("Staff club fund:%2f\n",total3);
+    	System.out.printf("Staff salary is:%2f\n",gross);
+    	System.out.printf("net salary is:%2f\n",net);
+	}    	
+}
---
+++ src/payroll/payslip.java
@@ -0,0 +1,20 @@
+package payroll;
+
+public class payslip {
+	public static void main (String[]args) {
+		Programmer pro;
+		AssistantProfessor AssPro;
+		AssociativeProfessor AsoPro;
+		Professor Prof;
+		pro=new Programmer("Pavan",300001,"chennai","pavan@gmail.com",9000001,600000);
+		AssPro=new AssistantProfessor ("Kalyan",600001,"chennai","kalyan@gmail.com",70000001,50000);
+		AsoPro=new AssociativeProfessor("Sumanth",8000001,"nellore","sumanth@gmail.com",700001,70000);
+		Prof=new Professor ("Reddy",9000001,"kadapa","reddy@gmail.com",2000001,900000);
+		pro.printAccount();
+		AssPro.printStatement();
+		AsoPro.printStatement();
+		Prof.printStatement();	
+	}
+	
+
+}
