ba80be321e781e6b614d3818dfbb3cfbfd3ca095
user3 <user3@administrator-HP-Pro-3330-MT>
2019-07-22 15:47:44 +0530

    calculation of payroll

---
+++ src/payroll/AssociateProfessor.java
@@ -0,0 +1,48 @@
+package payroll;
+
+public class AssociateProfessor extends Employee{
+	private double basic_pay;
+	public double da;
+	public double hra;
+	public double pf;
+	public double staff_club;
+	public double gross_salary;
+	public double net_salary;
+	
+	public AssociateProfessor()
+	{
+	 basic_pay=0;
+	}
+	
+	public AssociateProfessor(String n,long id,String ad,String mail,long mo,double bp)
+	{
+		basic_pay=bp;
+	}
+	
+	
+	public void Employee()
+	{
+		super.printEmployee();
+		System.out.println("enter the basic pay salary:"+basic_pay);
+	}
+	
+	public void calculation()
+	{
+		double da,hra,pf,staff_club;
+		da=0.97*basic_pay;
+		hra=0.1*basic_pay;
+		pf=1.2*basic_pay;
+		staff_club=10*basic_pay;
+		gross_salary=da+hra+pf+staff_club;
+		net_salary=basic_pay-(pf+staff_club);
+
+}
+public void printAssociateProfessor(){
+	
+	System.out.println("employee basic salary"+basic_pay);
+	System.out.println("employee gross salary"+ gross_salary);
+    System.out.println("employee net salary"+net_salary);
+}
+
+}
+
---
+++ src/payroll/PaySlip.java
@@ -0,0 +1,26 @@
+package payroll;
+
+public class PaySlip {
+
+	public static void main(String[] args) {
+		Employee emp;
+		Programmer prog;
+	    AssociateProfessor aspro;
+	    AssistantProfessor asspro;
+	    Professor pro;
+		    emp= new Employee("yogi",5000001,"chennai","account@gmail.com",8000000001l);
+			prog=new Programmer("Raja",300001,"Chennai","account@gmail.com",9000000001l,400000.0);
+
+			aspro=new AssociateProfessor("Kamal",600001,"Chennai","account@gmail.com",7000000001l,500000.00);
+			
+			asspro=new AssistantProfessor("Kala",800001,"Chennai","account@gmail.com",4000000001l,400000.00);
+			 
+			pro=new Professor("suri",400001,"myd","account@gmail.com",8000000001l,300000.00);
+			emp.printEmployee();
+			pro.printProfessor();			
+			aspro.printAssociateProfessor();
+			asspro.printAssistantProfessor();
+			prog.printProgrammer();
+	}
+
+}
