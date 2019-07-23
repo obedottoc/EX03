f9afe13d2a6e7cddd16e3ff24c548249da5776da
user2 <user2@simlab25>
2019-07-23 10:21:26 +0530

    ctreated payroll

---
+++ src/payroll/Calculation.java
@@ -0,0 +1,29 @@
+package payroll;
+
+public class Calculation {
+	public static void main(String[]args) {
+		Programmer Pro;
+		AssistantProfessor Asspro;
+		AssociativeProfessor Asopro;
+		Professor prof;
+		Pro=new Programmer("Raja",300001,"chennai","account@gmail.com",94074567l,60000);
+		
+		Asspro=new AssistantProfessor("mani",245678,"chennai","account@gmail.com",23456789l,23456);
+		
+		Asopro=new AssociativeProfessor("vijay",654321,"chennai","account@gmail.com",98765432l,898765);
+		
+		prof=new Professor("harsha",456789,"chennai","account@gmail.com",23456789l,234567);
+		
+		Pro.printAccount();
+		Asspro.printStatement();
+		Asopro.printStatement();
+		prof.printStatement();
+		
+		
+		
+		
+	
+		
+	}
+
+}
