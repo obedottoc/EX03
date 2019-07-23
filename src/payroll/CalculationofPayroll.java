ea460e15750a73767181c91ed494c1b584d4c289
gk81299 <52190227+gk81299@users.noreply.github.com>
2019-07-23 08:30:47 +0530

    created by 			kaarthikeyan.G

---
+++ src/payroll/CalculationofPayroll.java
@@ -0,0 +1,29 @@
+package payroll;
+public class CalculationofPayroll {
+
+	public static void main(String[] args) {
+		{
+			Employee emp1;
+			Programmer prog1;
+			AssistantProfessor astp1;
+			AssociateProfessor asop1;
+			Professor pro1;
+			
+			emp1=new Employee("Name",1001,"chennai","name@gmail.com",1111111111l);
+			prog1=new Programmer("Sanjay",1002,"chennai","sanjay@gmail.com",2222222222l,10000);		
+			astp1=new AssistantProfessor("AvinashRaja",1003,"chennai","avi@gmail.com",3333333333l,10000);
+			asop1=new AssociateProfessor("Lokesh",1004,"chennai","loki@gmail.com",4444444444l,20000);
+			pro1=new Professor("karthikeyan",1005,"chennai","kathik@gmail.com",5555555555l,40000);		
+			emp1.printaccount();
+			prog1.calculation();
+			astp1.calculation();
+			asop1.calculation();
+			pro1.calculation();
+			prog1.printstatement();
+			astp1.printstatement();
+			asop1.printstatement();
+			pro1.printstatement();
+		}
+
+	}
+}
---
+++ src/payroll/Employee.java
@@ -0,0 +1,51 @@
+/*This program is created by
+							Kaarthikeyan.G
+							email:gk81299@gmail.com*/
+package payroll;
+import java.util.Scanner;
+public class Employee 
+	{
+		protected String emp_name;
+		protected long emp_id;
+		protected String address;
+		protected String mail_id;
+		protected long mobile_no;
+	public Employee()
+	{
+		emp_name="no name";
+		emp_id=10001;
+		address="not given";
+		mobile_no=80001;		
+	}
+	public Employee(String n,long id,String ad,String mail,long mo)
+	{
+		emp_name=n;
+		emp_id=id;
+		address=ad;
+		mail_id=mail;
+		mobile_no=mo;
+	}
+	public void read()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the employee id:");
+		emp_id=sc.nextInt();
+		System.out.println("enter the employee name:");
+		emp_name=sc.next();
+		System.out.println("enter the mail id:");
+		mail_id=sc.next();
+		System.out.println("enter the mobile no:");
+		mobile_no=sc.nextInt();
+		System.out.println("enter the address:");
+		address=sc.next();
+	}
+	public void printaccount()
+	{
+	System.out.println("name:"+emp_name);
+	System.out.println("account id:"+emp_id);
+	System.out.println("address:"+address);
+	System.out.println("+mail:"+mail_id);
+	System.out.println("mobile:"+mobile_no);
+					
+	}
+}
