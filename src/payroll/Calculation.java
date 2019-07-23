ceccf2e2cb5a8599dba473ccd7892688212b10f0
user2 <user2@simlab>
2019-07-23 10:35:52 +0530

    This Program is to calculate gross salary and net salary Created by Faizul faizulsmart10@gmail.com

---
+++ src/payroll/Calculation.java
@@ -0,0 +1,27 @@
+//Program to represent payroll
+//Created by Faizul
+//faizulsmart10@gmail.com
+package payroll;
+
+public class Calculation {
+      public static void main(String[]args) {
+    	  Employee emp;
+    	  Programer pro;
+    	  AssistantProfessor Asspro;
+    	  AssociateProfessor Asopro;
+    	  Professor prof;
+    	  
+    	  emp=new Employee("Hariharan",5022,"Chennai","harirahul@gmail.com",87346);
+    	  pro=new Programer("Faizul",5016,"Karur","faizulsmart@gmail.com",75167,50000);
+    	  Asspro=new AssistantProfessor("Akhshy",5006,"Chennai","akhshyganeshb@gmail.com",91886,55000);
+    	  Asopro=new AssociateProfessor("Sanjai",5019,"Chennai","gsanjaikumar@gmail.com",19764,60000);
+		  prof=new Professor("Abhijith",5002,"Chennai","abhijithabhi@gmail.com",54533,65000);
+		  pro.printAccount();	
+		  Asspro.printStatement();
+		  Asopro.printStatement();
+		  prof.printStatement();
+		
+
+    	  
+      }
+}
---
+++ src/payroll/Employee.java
@@ -0,0 +1,49 @@
+//Program to represent payroll
+//Created by Faizul
+//faizulsmart10@gmail.com
+package payroll;
+
+public class Employee {
+
+	protected String employee_name;
+	protected long employee_id;
+	protected String address;
+	protected long mobile_no;
+	protected String mail_id;
+	
+	public Employee()
+	{
+		employee_name="no name";
+		employee_id=5001;
+		address="not given";
+		mail_id="not given";
+		mobile_no=2122;
+		
+	}
+	
+	public Employee(String n,long id,String add,String mail,long num)
+	{
+		employee_name=n;
+		employee_id=id;
+		address=add;
+		mail_id=mail;
+		mobile_no=num;
+	}
+	
+	public void printAccount()
+	
+	{
+		System.out.println("Name:"+employee_name);
+		System.out.println("Account Id:"+employee_id);
+		System.out.println("Address:"+address);
+		System.out.println("Email:"+mail_id);
+		System.out.println("Mobile:"+mobile_no);
+	}
+}
+		
+		
+	
+	
+	
+	
+	
