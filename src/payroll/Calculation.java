c8c2a2430a2931965b408c97bc6546280e523df6
lakshmi <lakshmi@172.17.82.176>
2019-07-23 11:15:16 +0530

    employee

---
+++ src/payroll/Calculation.java
@@ -0,0 +1,27 @@
+package payroll;
+
+import payroll.Assistantprofessor;
+import payroll.Associateprofessor;
+import payroll.Employee;
+import payroll.Professor;
+import payroll.Programmer;
+    public class Calculation 
+    {
+        public static void main(String[]args) {
+            Employee emp1;
+            Assistantprofessor asp1;
+            Associateprofessor ap1;
+            Professor p1;
+            Programmer pro1;
+        emp1=new Employee("lokesh",4001,"chennai","lk@gmail.com",924518268936l);
+        asp1=new Assistantprofessor("avinash",4553,"chennai","avi@gmail.com",995426131469l,5000.00);
+        ap1=new Associateprofessor("gk",5004,"chennai","gk@gmail.com",98754623122l,7000.00);
+        p1=new Professor("agnal",7893,"chennai","ag@gmail.com",984512847l,9000.00);
+        pro1=new Programmer("ak",5542,"chennai","ak@gmail.com",664452221l,10000.00);
+        emp1.printEmployee();
+        asp1.printEmployee();
+        ap1.printEmployee();
+        p1.printEmployee();
+        
+        }
+    }
---
+++ src/payroll/Employee.java
@@ -0,0 +1,38 @@
+package payroll;
+public class Employee {
+    protected String Emp_names;
+    protected long Emp_id;
+    protected String address;
+    protected String mail_id;
+    protected long mobile_no;
+    
+    public Employee()
+    {
+        Emp_names ="avinash";
+        Emp_id =1500125;
+        address ="not given";
+        mail_id="not given";
+        mobile_no =9445223556l;
+    }
+    
+    public Employee(String n,long id,String ad,String mail,long num)
+    {
+        Emp_names =n;
+        Emp_id =id;
+        address=ad;
+        mail_id=mail;
+        mobile_no=num;
+    }
+    
+    public void printEmployee()    
+    {
+     System.out.println("name:"+Emp_names);
+     System.out.println("id:"+Emp_id);
+     System.out.println("address:"+address);
+     System.out.println("email:"+mail_id);
+     System.out.println("mobile:"+mobile_no);
+    } 
+   
+    
+    
+}
