c5c3b7e7a0a9e0ce308ed80a45dcde9eb3e01fc8
sumanth <sumanth@E5-571>
2019-08-04 12:27:05 +0530

    created the payroll

---
+++ src/Payroll/Calculation.java
@@ -0,0 +1,24 @@
+package Payroll;
+
+public class Calculation {
+
+public static void main(String[] args) {
+   Programmer pro;
+AssistantProfessor Asspro;
+AssociativeProfessor Asopro;
+Professor prof;
+pro=new Programmer("uday",250076,"Chennai","account@gmail.com",667457845,786547);
+
+Asspro=new AssistantProfessor("hari",698700,"Chennai","account@gmail.com",79857001l,89324);
+
+Asopro=new AssociativeProfessor("suresh",960654,"Chennai","account@gmail.com",958574785,88769);
+
+prof=new Professor("rajesh",900786,"chennai","account@gmail.com",895689561,675675);
+pro.printStatement();
+Asspro.printStatement();
+Asopro.printStatement();
+prof.printStatement();
+}
+
+}
+
