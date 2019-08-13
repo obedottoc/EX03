705c7fccb5cf37d4d804968cc6ff31ed6f808d7d
Karthik Bhaskar <Karthik Bhaskar@DESKTOP-JHFE8LL>
2019-08-13 15:57:00 +0530

    pdf for record

---
+++ src/payroll/AssistantProfessor.java
@@ -0,0 +1,47 @@
+/**
+ * program to represent BP of Assistant professor
+ * @author Karthik Bhaskar
+ * karthikbhaskar2000@gmail.com
+ */
+package payroll;
+
+public class AssistantProfessor extends Employee {
+       private double basicpay;
+public AssistantProfessor()
+{
+      basicpay=0;
+}
+public AssistantProfessor(String n,long id,String add,String mail,long num,double BP)
+{
+	super(n,id,add,mail,num);
+	basicpay=BP;
+}
+public void printAccount()
+{
+	super.printaccount();
+	System.out.println("Basic pay:"+basicpay);
+}
+public void printStatement()
+{
+	double total;
+	double total1;
+	double total2;
+	double total3;
+	double gross;
+	double net;
+	printAccount();
+	total=basicpay*0.97;
+	total1=basicpay*0.1;
+	total2=basicpay*0.12;
+	total3=basicpay*0.001;
+	gross=total+total1+total2+total3;
+	net=gross-total2-total3;
+	System.out.printf("duty allowance(DA):%.2f\n",total);
+	System.out.printf("HRA:%.2f\n",total1);
+	System.out.printf("PF:%2f\n",total2);
+	System.out.printf("staff club fund:%.2f\n",total3);
+	System.out.printf("gross salary is:%.2f\n",gross);
+	System.out.printf("net salary is:%.2f\n",net);
+	System.out.println("------------------------------------------------------------------------------------------------------------");
+	}
+}
---
+++ src/payroll/Employee.java
@@ -0,0 +1,40 @@
+/**
+  * program to represent employee details
+ * @author Karthik Bhaskar
+ * karthikbhaskar2000@gmail.com
+ */
+
+package payroll;
+
+public class Employee {
+     protected String emp_name;
+     protected long emp_id;
+     protected String address;
+     protected String mail_id;
+     protected long mobile_no;
+     
+public Employee()    
+{
+	emp_name="name";
+	emp_id=123123;
+	address="not given";
+	mail_id="not given";
+	mobile_no=91637543;	
+}
+public Employee(String n,long id,String add,String mail,long num)
+{
+	emp_name=n;
+	emp_id=id;
+	address=add;
+	mail_id=mail;
+	mobile_no=num;
+}
+public void printaccount()
+{
+	System.out.println("Name of the employee:"+emp_name);
+	System.out.println("Employee ID:"+emp_id);
+	System.out.println("Address:"+address);
+	System.out.println("Mail ID of the employee:"+mail_id);
+	System.out.println("Mobile number of the employee:"+mobile_no);
+	}
+}
